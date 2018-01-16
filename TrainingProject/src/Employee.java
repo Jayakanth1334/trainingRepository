5ebf33b49e4e9bee0b8b0512e247c6307a42a8f4
jalogana <jalogana@LSG0100338.corp.capgemini.com>
2018-01-16 15:48:07 +0800

    Employee System Checker program

---
+++ TrainingProject/src/Employee.java
@@ -0,0 +1,61 @@
+
+public class Employee {
+	
+	private String firstName;
+	private String lastName;
+	private String salary;
+	private String grade;
+	private String joiningMonth;
+	
+	static long count =0;
+	
+	
+	{
+		count++;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getJoiningMonth() {
+		return joiningMonth;
+	}
+	public void setJoiningMonth(String joiningMonth) {
+		this.joiningMonth = joiningMonth;
+	}
+	public long getCount() {
+		return count;
+	}
+	
+
+
+	}
+	
+	
+	
+	
+	
+	
+
---
+++ TrainingProject/src/EmployeeSystem.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeSystem {
+
+	public static void main(String[] args) {
+
+		
+
+		
+		
+
+		generateEmployee("tom", "jerry", "A", "December", "12213");
+		generateEmployee("kamal", "Hassan", "A", "Novemeber","32323");
+		
+		System.out.println(Employee.count);
+
+	
+
+	}
+
+	private static void generateEmployee(String firstName, String lastName, String grade, String joiningMonth,
+			String salary) {
+
+	
+
+		Employee emp = new Employee();
+		
+		emp.setFirstName(firstName);
+		emp.setLastName(lastName);
+		emp.setGrade(grade);
+		emp.setJoiningMonth(joiningMonth);
+		emp.setSalary(salary);
+		
+	System.out.println(emp.getFirstName()+" "+emp.getGrade());
+		
+		
+
+	}
+
+}
